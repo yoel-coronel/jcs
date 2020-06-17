999641d1d861eccbf7727f7f988641d82f9afc4e
Yoel <yoe.coar@gmail.com>
2020-06-17 01:08:12 -0500

    Rutas de actualizar contraseña y recuperar cuenta

--- routes/web.php
+++ routes/web.php
@@ -9,15 +9,37 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/inicio', 'HomeController@index')->name('home');
+Route::middleware(['guest'])->group(function (){
 
-Route::middleware(['auth'])->group(function (){//rutas autenticadas
+    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Auth\LoginController@login');
 
-    //Route::view('/marcas', 'marcas.index')->name('marcas.index');
+
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+});
+
+Route::middleware(['auth'])->group(function (){
+
+    Route::get('/inicio', 'HomeController@index')->name('home');
+
+
+    Route::get('password/confirm', 'Auth\LoginController@Auth\ConfirmPasswordController@showConfirmForm')->name('password.confirm');
+    Route::post('password/confirm', 'Auth\LoginController@Auth\ConfirmPasswordController@confirm');
+
+    Route::view('/marcas', 'marcas.index')->name('marcas.index');
+
+
+    Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 });
 
-Route::view('/marcas', 'marcas.index')->name('marcas.index');
+
+
+
 
 Route::get('/datos','HomeController@datos')->name('datos');
 
